9b8ceab3f53cdd8ae7077389a28b0e17fbf27567
deojo <50883840+deojo@users.noreply.github.com>
2023-03-24 09:15:53 -0700

    Add files via upload

--- bias_toxicity_analysis/anthropic/detoxify_anthropic_hfds.py
+++ bias_toxicity_analysis/anthropic/detoxify_anthropic_hfds.py
@@ -1,154 +1,126 @@
 
-
 #!/usr/bin/env python
 # coding: utf-8
 # Author: DJoshi
 
-import json
+
 from detoxify import Detoxify
 from datasets import load_dataset
 import pandas as pd
 import numpy as np
-import evaluate
-import re
-from transformers import T5Tokenizer, AutoModel,AutoModelForSeq2SeqLM
-from torch import Tensor
-import torch
 
-import warnings
-warnings.filterwarnings("ignore")
-
+LAST_ASSISTANT = True
 #MODEL_TYPE = "original"
-MODEL_TYPE = 'unbiased'
+MODEL_TYPE = 'unbiased
 
-def match_category(text, category, category_variants):
-    matches = []
-    text = text.lower()
-    for variant in category_variants:
-        match = re.search(f"\\b{variant.lower()}\\b", text)
-        if match:
-            matches.append(variant)
-            break
+def get_assistant_text(text):
+    splits = text.split('\n')
+    
+    assistants = []
+    assistant_text = ''
+    assistant = False
+    for split in splits:
+        split = split.strip()
+        if len(split) > 0:
+            if split.startswith('Assistant:'):
+                assistant = True
+            elif split.startswith('Human:'):
+                
+                assistant = False
+                if len(assistant_text) > 0:
+                    assistants.append(assistant_text)
+                assistant_text = ''
+            
+            if assistant == True:
+                if len(assistant_text) > 0:
+                    assistant_text += '\n'
+                
+                assistant_text += split
+    
+    if len(assistant_text) > 0:
+        assistants.append(assistant_text)
         
-    return matches
+    return assistants
 
-def get_matches_rec(text, parent_categories, category, descriptors, matches):
-    if len(parent_categories) > 0:
-        parent_categories += ' -> '
-    parent_categories = parent_categories + category
-    for key, value in descriptors.items():
-        if type(value) is dict:
-            get_matches_rec(text, parent_categories, key, value, matches)
-        else:
-            match = match_category(text, key, value)
-            if len(match) > 0:
-                matches.append({'parent_categories': parent_categories, 'category': key, 'matches': match})
+def get_assistant_texts(texts):
+    assistant = []
+    for txt in texts:
+        assistant_texts = get_assistant_text(txt)
+        if LAST_ASSISTANT == True:
+            assistant_texts = [assistant_texts[-1]]
+        assistant.append("\n".join(assistant_texts))
+    return assistant
+
+def detoxify(batch, detoxify_model):
+    items = batch.items()
+    chosen = get_assistant_texts(batch.get('chosen'))
+    rejected = get_assistant_texts(batch.get('rejected'))
+    
+    batch['chosen_assistant'] = chosen
+    batch['rejected_assistant'] = rejected
+    
+    chosen_toxicity = detoxify_model.predict(chosen)
+    for key, value in chosen_toxicity.items():
+        batch[f'chosen_{key}'] = value
         
-def get_matches(text, descriptors):
-    matches = []
-    get_matches_rec(text, "", "", descriptors, matches)
-    return matches
-
-def prepend_prompt(prompts):
-    for i in range(len(prompts)):
-        idx = prompts[i].rindex('Assistant:')
-        prompts[i] = "continue the conversation as an Assistant: " + prompts[i][0:idx]
-      #    prompts[i] = prompts[i]
-    return prompts
-
-#def detoxify(batch, tokenizer, model, detoxify_model, regard_model, descriptors):
-def detoxify(batch):
-    prompts = prepend_prompt(batch.get('prompt'))
-    
-    decoder_prefix: str = ["Assistant:"]* len(prompts)
-
-    # need to skip special tokens or it will add </s>, messing up insertion as decoder_input_ids
-    decoder_inputs = tokenizer(decoder_prefix, return_tensors="pt", add_special_tokens=False)
-
-    # insert the decoder start token id b/c apparently this override
-    decoder_input_ids: Tensor = torch.cat([
-        torch.tensor([[model.config.decoder_start_token_id]]* len(prompts)),  # note batch of 1
-        decoder_inputs.input_ids
-    ], dim=1).to(model.device)
-    
-    inputs = tokenizer(prompts, return_tensors="pt", padding=True)
-   
-    output_ids = model.generate(
-        input_ids=inputs.input_ids.to(model.device),
-        attention_mask=inputs.attention_mask.to(model.device),
-        decoder_input_ids = decoder_input_ids
-    )
- #    output_ids = model(**inputs)
-    
-    # skip_special_tokens=True cleans <pad> and </s>
-    outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    batch['output'] = outputs
-    
-    toxicity = detoxify_model.predict(outputs)
-    for key, value in toxicity.items():
-        batch[f'{key}'] = value
-        
-    values = np.array(list(toxicity.values())).T.tolist()
-    keys = list(toxicity.keys())
-    batch['max_toxity_key'] = []
-    batch['max_toxity_value'] = []
-    batch['toxic'] = []
+    values = np.array(list(chosen_toxicity.values())).T.tolist()
+    keys = list(chosen_toxicity.keys())
+    batch['chosen_max_toxity_key'] = []
+    batch['chosen_max_toxity_value'] = []
+    batch['chosen_toxic'] = []
     for i in range(len(values)):
         max_values = max(zip(keys, values[i]))
-        batch['max_toxity_key'].append(max_values[0])
-        batch['max_toxity_value'].append(max_values[1])
-        batch['toxic'].append(True if float(max_values[1]) >= 0.01 else False)
+        batch['chosen_max_toxity_key'].append(max_values[0])
+        batch['chosen_max_toxity_value'].append(max_values[1])
+        batch['chosen_toxic'].append(True if float(max_values[1]) >= 0.01 else False)
         
+    rejected_toxicity = detoxify_model.predict(rejected)
+    for key, value in rejected_toxicity.items():
+        batch[f'rejected_{key}'] = value
         
-    results = regard_model.compute(data = outputs)['regard']
-    batch['regard'] = results
-    batch['regard_neutral'] = []
-    batch['regard_negative'] = []
-    batch['regard_positive'] = []
-    batch['regard_other'] = []
-    
-    for result in results:
-        for item in result:
-            if item['label'] == 'neutral':
-                batch['regard_neutral'].append(item['score'])
-            elif item['label'] == 'positive':
-                batch['regard_positive'].append(item['score'])
-            elif item['label'] == 'negative':
-                batch['regard_negative'].append(item['score'])
-            elif item['label'] == 'other':
-                batch['regard_other'].append(item['score'])
-            
-    batch['bias_matches'] = []
-    for choseen_assistant_text in outputs:
-        matches = json.dumps(get_matches(choseen_assistant_text, descriptors))
-        batch['bias_matches'].append(matches)
+    values = np.array(list(rejected_toxicity.values())).T.tolist()
+    keys = list(rejected_toxicity.keys())
+    batch['rejected_max_toxity_key'] = []
+    batch['rejected_max_toxity_value'] = []
+    batch['rejected_toxic'] = []
+    for i in range(len(values)):
+        max_values = max(zip(keys, values[i]))
+        batch['rejected_max_toxity_key'].append(max_values[0])
+        batch['rejected_max_toxity_value'].append(max_values[1])
+        batch['rejected_toxic'].append(True if float(max_values[1]) >= 0.01 else False)
+        
     return batch
 
 
-def process(dataset, split, descriptors):
+def process(dataset, split):
     ds = dataset[split]
-#    ds = dataset.map(detoxify, fn_kwargs={'tokenizer': tokenizer, 'model': model, 'detoxify_model': detoxify_model, 'regard_model': regard_model, 'descriptors': descriptors}, batched=True, batch_size = 5)
-    ds = ds.map(detoxify, batched=True, batch_size = 5)
-  #  print(ds)
+    ds = dataset.map(detoxify, fn_kwargs={'detoxify_model': detoxify_model}, batched=True, batch_size = 50)
 
-    dataset_name = f'reward_model_anthropic'
-    
+    dataset_name = f'detoxify_{model_type}_hhrlhf_'
+    if LAST_ASSISTANT == True:
+        dataset_name += f'last_assistant'
+    else:
+        dataset_name += f'assistant'
+
+
     ds.to_csv(f'./{dataset_name}_{split}.csv')
 
+
     print("Uploading to Huggingface")
-    ds.push_to_hub(f'{dataset_name}', token ='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')
+    ds.push_to_hub(f'dataset_name', token ='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')
 
-with open('bias_category_descriptors.json') as json_file:
-    descriptors = json.load(json_file)
 
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-model = AutoModelForSeq2SeqLM.from_pretrained("Deojoandco/anthropic_hh_reward_model", device_map="auto")
+detoxify_model = Detoxify(model_type, device='cuda')
+dataset = load_dataset("Anthropic/hh-rlhf")
+process(dataset, 'train')
+process(dataset, 'test')
 
-detoxify_model = None
-regard_model= None
-detoxify_model = Detoxify(MODEL_TYPE, device='cuda')
-regard_model = evaluate.load("regard", module_type="measurement")
 
-dataset = load_dataset("Deojoandco/anthropic-hh-rlhf")
-process(dataset, 'train', descriptors)
-#process(dataset, 'test', descriptors)
+
+
+
+# In[ ]:
+
+
+
+
